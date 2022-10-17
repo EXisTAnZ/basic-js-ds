ce580844729399b2f831fadb8368265ab7854247
EXisTAnZ <ozim_ssspam@mail.ru>
2022-10-17 14:14:07 +0300

    feat: copy hasData to findData with change return type 	modified:   src/binary-search-tree.js

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -40,37 +40,46 @@ class BinarySearchTree {
   }
 
   has(data) {
-    function hasData(node,data) {
-      if(!node)
+    function hasData(node, data) {
+      if (!node)
         return false;
-      if(node.data === data)
+      if (node.data === data)
         return true;
       if (node.data < data)
         return hasData(node.right, data)
       return hasData(node.left, data)
     }
-    return hasData(this.rootNode,data);
+    return hasData(this.rootNode, data);
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  find(data) {
+    function findData(node, data) {
+      if (!node)
+        return null;
+      if (node.data === data)
+        return node;
+      if (node.data < data)
+        return findData(node.right, data)
+      return findData(node.left, data)
+    }
+    return findData(this.rootNode, data);
   }
+}
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+remove(data) {
+  throw new NotImplementedError('Not implemented');
+  // remove line with error and write your code here
+}
 
-  min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+min() {
+  throw new NotImplementedError('Not implemented');
+  // remove line with error and write your code here
+}
 
-  max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+max() {
+  throw new NotImplementedError('Not implemented');
+  // remove line with error and write your code here
+}
 }
 
 module.exports = {
