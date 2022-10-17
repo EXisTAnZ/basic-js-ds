ed49178c1299aefeafb140a5cf3877301878ac2a
EXisTAnZ <ozim_ssspam@mail.ru>
2022-10-17 14:45:26 +0300

    feat: implemented min and max methods 	modified:   src/binary-search-tree.js

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
@@ -64,21 +64,42 @@ class BinarySearchTree {
     }
     return findData(this.rootNode, data);
   }
-}
 
-remove(data) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+  remove(data) {
+    function removeData(node, data) {      
+    if (!node)
+      return null;
+    if (node.data === data)
+      return node;
+    if (node.data < data)
+      return removeData(node.right, data)
+    return removeData(node.left, data)
+  }
+  return removeData(this.rootNode, data);
+
 }
 
 min() {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+  function minNode(node) {
+    if (!node)
+      return null;
+    if (!node.left)
+      return node;
+    return minNode(node.left);
+  }
+  return minNode(this.rootNode).data;
+
 }
 
 max() {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+  function maxNode(node) {
+    if (!node)
+      return null;
+    if (!node.right)
+      return node;
+    return maxNode(node.right);
+  }
+  return maxNode(this.rootNode).data;
 }
 }
 
